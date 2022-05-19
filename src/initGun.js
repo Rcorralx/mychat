e1a9d1dc6fc14d2dffd12f7e83ed1cbdbba94a4c
Rcorralx <rcorralx@gmail.com>
2022-05-19 15:31:05 -0600

    second round

--- src/initGun.js
+++ src/initGun.js
@@ -8,12 +8,12 @@ if (process.env.NODE_ENV === "development") {
 } else {
   peers = [
     // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
-    "https://www.raygun.live/gun",
-    "https://gunmeetingserver.herokuapp.com/gun",
-    "https://gun-us.herokuapp.com/gun",
-    "https://gun-eu.herokuapp.com/gun",
+   //  "https://www.raygun.live/gun",
+ //    "https://gunmeetingserver.herokuapp.com/gun",
+  //  "https://gun-us.herokuapp.com/gun",
+ //   "https://gun-eu.herokuapp.com/gun",
     // My own relay peer
-    "https://phrassed.com/gun",
+//    "https://phrassed.com/gun",
   ];
 }
 
