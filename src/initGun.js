a43a6cacf255d93d15270ea83f302b932d96bd33
Rcorralx <rcorralx@gmail.com>
2022-05-19 19:47:11 -0600

    round-6

--- src/initGun.js
+++ src/initGun.js
@@ -4,7 +4,7 @@ import "gun/lib/webrtc";
 let peers;
 
 if (process.env.NODE_ENV === "development") {
-//  peers = ["https://mychat-x1.vercel.app/gun"];
+//  peers = ["http://localhost:3000/gun"];
 } else {
  peers = [
     // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
