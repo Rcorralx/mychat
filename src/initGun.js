7d206709f39505497c10940622a15b994b310cea
Rcorralx <rcorralx@gmail.com>
2022-05-20 10:48:26 -0600

    round-15

--- src/initGun.js
+++ src/initGun.js
@@ -4,13 +4,12 @@ import "gun/lib/webrtc";
 let peers;
 
 if (process.env.NODE_ENV === "development") {
-  peers = ["http://localhost:8765/gun", "https://mychat-x1.vercel.app/gun"];
+  peers = ["http://localhost:8765/gun"];
 } else {
  peers = [
-    // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
-   //  "https://www.raygun.live/gun",
-     "https://mychat-x1.vercel.app/gun",
-     "https://gunmeetingserver.herokuapp.com/gun",
+    //Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
+   // "https://www.raygun.live/gun",
+   // "https://gunmeetingserver.herokuapp.com/gun",
   //  "https://gun-us.herokuapp.com/gun",
  //   "https://gun-eu.herokuapp.com/gun",
     // My own relay peer
